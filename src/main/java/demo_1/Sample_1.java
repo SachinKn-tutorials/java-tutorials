24d9594e2e5c06ca51530e5e7406546189fe13f4
SachinKn <106311617+sachinknsachi@users.noreply.github.com>
2024-09-28 12:39:33 +0530

    Added demo files

---
+++ src/main/java/demo_1/Sample_1.java
@@ -0,0 +1,5 @@
+package demo_1;
+
+public class Sample_1 {
+    public static int x = 10;
+}
---
+++ src/test/java/demo_2/Sample_2.java
@@ -0,0 +1,11 @@
+package demo_2;
+
+import demo_1.Sample_1;
+
+public class Sample_2 {
+
+    public static void main(String[] args) {
+        System.out.println(Sample_1.x);
+    }
+
+}
